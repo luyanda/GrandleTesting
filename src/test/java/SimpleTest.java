8356d00931d582733581e040696c3fc1361161cb
luyanda <luyandambodla@gmail.com>
2018-03-20 01:34:45 +0200

    Fourth Commitment

--- src/test/java/SimpleTest.java
+++ src/test/java/SimpleTest.java
@@ -1,10 +1,47 @@
 package test.java;
 
-import org.junit.jupiter.api.Test;
+import main.java.Person;
+import main.java.School;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
 
 public class SimpleTest {
+    Person person;
+    School school;
+
+    @Before
+    public void open() {
+        person = new Person();
+        school = new School();
+    }
+    @After
+    public void closer(){
+        person =  null;
+        school = null;
+    }
     @Test
     public void test1(){
         System.out.print("Test one");
+    }
+
+    @Test( expected =ArrayIndexOutOfBoundsException.class)
+    public void Test2(){
+        person.setName("Sisa");
+        Assert.assertNull(person.getName());
+    }
+
+    @Test( expected =ArrayIndexOutOfBoundsException.class)
+    public void Test3(){
+        school.setSchoolName("Nongeke");
+        Assert.assertNull(school.getSchoolName());
+    }
+
+    @Test
+    public void Test4(){
+        person.setName("Sisa");
+        school.setSchoolName("NMU");
+        Assert.assertSame(person.getName(),school.getSchoolName());
     }
 }
